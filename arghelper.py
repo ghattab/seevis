9867bb7526e846ef6696ca2a7bd89144080438d1
Georges Hattab <ghattab@users.noreply.github.com>
2018-05-24 11:24:34 +0200

    PEP8 style fix

--- arghelper.py
+++ arghelper.py
@@ -4,28 +4,31 @@
 import argparse
 import os
 
+
 def inputfile(path):
     if not path.endswith('.csv'):
-        raise argparse.ArgumentTypeError('argument filename must be of type *.csv')
+        raise argparse.ArgumentTypeError('argument '
+                                         'filename must be of type *.csv')
     return path
+
 
 def check_range(arg):
     try:
         value = int(arg)
     except ValueError as err:
-       raise argparse.ArgumentTypeError(str(err))
+        raise argparse.ArgumentTypeError(str(err))
     if value < 1 or value > 3:
         message = "Expected (1, 2 or 3), got value = {}".format(value)
         raise argparse.ArgumentTypeError(message)
     return value
 
-def inputdir(parser, path):
-  if not os.path.isdir(path):
-    message = "directory:{0} is not a valid path".format(path)
-    parser.error(message)
-  elif os.access(path, os.R_OK):
-    return path
-  else:
-    message = "directory:{0} is not a readable directory".format(path)
-    raise argparse.ArgumentError(message)
 
+def inputdir(parser, path):
+    if not os.path.isdir(path):
+        message = "directory:{0} is not a valid path".format(path)
+        parser.error(message)
+    elif os.access(path, os.R_OK):
+        return path
+    else:
+        message = "directory:{0} is not a readable directory".format(path)
+        raise argparse.ArgumentError(message)
